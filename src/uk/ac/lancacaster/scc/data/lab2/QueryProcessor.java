a592f1062cfe0b4b4fc4b028aaaa883b6a5f0e84
Matthew Rowe <m.rowe@lancaster.ac.uk>
2014-02-18 14:59:23 +0000

    Updated lab class structure

---
+++ src/uk/ac/lancacaster/scc/data/lab2/QueryProcessor.java
@@ -0,0 +1,85 @@
+package uk.ac.lancacaster.scc.data.lab2;
+
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
+
+/**
+ * Author: Matthew Rowe
+ * Email: m.rowe@lancaster.ac.uk
+ * Date / Time : 10/02/2014 / 14:31
+ */
+public class QueryProcessor {
+	// Query for the list of alumni celebrities
+    public void runQuery() {
+    	//"PREFIX owl: <http://dbpedia.org/ontology/almaMater>"dbpedia-owl:almaMater ;
+        
+        String query ="select ?Celeb where {?Celeb  <http://dbpedia.org/ontology/almaMater> <http://dbpedia.org/resource/Lancaster_University> }" ;
+        		//"select distinct ?Concept where {[] a ?Concept} LIMIT 100";
+        //for the full list of only locomotives - select ?Locomotives where {?Locomotives  <http://dbpedia.org/ontology/designer> <http://dbpedia.org/resource/Nigel_Gresley> }
+
+        // this the endpoint of the SPARQL server
+        String sparqlEndpointURL =  "http://dbpedia.org/sparql";
+
+        QueryExecution qexec = QueryExecutionFactory.sparqlService(sparqlEndpointURL,query);
+        ResultSet results = qexec.execSelect() ;
+        for ( ; results.hasNext() ; )
+        {
+            // get the next solution from the list
+           QuerySolution soln = results.nextSolution();
+
+            // get the variable value from the list
+           String Celeb = soln.get("?Celeb").toString();
+       
+        
+            // output the Celeb
+           	System.out.println(Celeb);
+           	
+         
+        }
+        qexec.close();
+      //LINE SPACE TO SEPERATE runQuery output from runQueryB
+       	System.out.println("");
+    }
+  
+    // Query for locomotives and their withdrawn dates
+    public void runQueryB() {
+    	//"PREFIX owl: <http://dbpedia.org/ontology/almaMater>"dbpedia-owl:almaMater ;
+        
+        String query = "select ?Locomotives ?Wdate where {?Locomotives  <http://dbpedia.org/ontology/designer> <http://dbpedia.org/resource/Nigel_Gresley>.  ?Locomotives  <http://dbpedia.org/property/withdrawndate> ?Wdate}" ;
+        		//"select distinct ?Concept where {[] a ?Concept} LIMIT 100";
+        //
+
+        // this the endpoint of the SPARQL server
+        String sparqlEndpointURL =  "http://dbpedia.org/sparql";
+
+        QueryExecution qexec = QueryExecutionFactory.sparqlService(sparqlEndpointURL,query);
+        ResultSet results = qexec.execSelect() ;
+        for ( ; results.hasNext() ; )
+        {
+            // get the next solution from the list
+           QuerySolution soln = results.nextSolution();
+
+            // get the variable value from the list
+           String Locomotives = soln.get("?Locomotives").toString();
+           String Wdate = soln.get("?Wdate").toString();
+        
+            // output the Locomotives and Wdate
+          
+           	System.out.println(Locomotives +" withdrawn date; " + Wdate );
+           	System.out.println("");//space for clarity
+         
+        }
+        qexec.close();
+    }
+    	
+    
+    public static void main(String[] args) {
+
+        QueryProcessor processor = new QueryProcessor();
+        processor.runQuery();
+        processor.runQueryB();
+    }
+
+}
---
+++ src/uk/ac/lancaster/scc/data/GraphAnalyser.java
@@ -0,0 +1,118 @@
+package uk.ac.lancaster.scc.data;
+
+import edu.uci.ics.jung.algorithms.metrics.Metrics;
+import edu.uci.ics.jung.algorithms.scoring.BetweennessCentrality;
+import edu.uci.ics.jung.algorithms.scoring.ClosenessCentrality;
+import edu.uci.ics.jung.algorithms.scoring.HITS;
+import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.graph.SparseMultigraph;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Author: Matthew Rowe
+ * Email: m.rowe@lancaster.ac.uk
+ */
+public class GraphAnalyser {
+
+    public void analyseGraph() {
+
+        // load the graph from the file
+
+        // point the graph location at the file
+//        String fileLocation = "/Users/mrowe/Documents/Teaching/SCC413/Labs/Lab3/stage1-out.ego-facebook.tsv";
+        String fileLocation = "/Users/mrowe/Documents/Teaching/SCC413/Labs/Lab3/stage2-out.ucidata-zachary.tsv";
+
+        // build the graph object from each edge in the file
+
+        // create a new DirectedGraph object to store the social network within
+        Graph<String, String> graph = new SparseMultigraph<String, String>();
+        int edgeIndex = 1;
+
+        try {
+
+            String sCurrentLine;
+            BufferedReader br = new BufferedReader(new FileReader(fileLocation));
+
+            while ((sCurrentLine = br.readLine()) != null) {
+
+                // split the line by the token delimiter (this could be either a \t or a space
+                String[] tokens = sCurrentLine.split(" ");
+
+                // the first token denotes the id of the first node
+                String sourceNode = tokens[0];  // from
+
+                // the second token denotes the id of the second node
+                String targetNode = tokens[1];  // to
+
+                // add the nodes to the graph object
+                if(graph.containsVertex(sourceNode))
+                    graph.addVertex(sourceNode);
+
+                if(graph.containsVertex(targetNode))
+                    graph.addVertex(targetNode);
+
+                // add the edge to the graph
+                String edgeLabel =  "Edge_" + edgeIndex;
+                graph.addEdge(edgeLabel,sourceNode,targetNode);
+
+                // increment the edge count index
+                edgeIndex++;
+            }
+
+            // measure the in-degree of a given node - you need to adapt this part for exercise 1 to return all nodes with an indegree greater than 0
+//            String nodeID = "1";
+//            int nodeInDegree = graph.inDegree(nodeID);
+//            System.out.println(nodeID + " = " + nodeInDegree);
+//
+//
+//            // measure the clustering coefficient of nodes within the network
+//            Map<String,Double> clusteringCoefficients = Metrics.clusteringCoefficients(graph);
+//            for (String node : clusteringCoefficients.keySet()) {
+//                if(clusteringCoefficients.get(node) > 0)
+//                    System.out.println(node + " = " + clusteringCoefficients.get(node));
+//            }
+
+            // which node is the most central in the network
+//            BetweennessCentrality centrality = new BetweennessCentrality(graph);
+//            for (String node : graph.getVertices()) {
+//                System.out.println(node + " = " + centrality.getVertexScore(node));
+//            }
+
+            double maxValue = 0;
+            String maxNode = "";
+            ClosenessCentrality centrality = new ClosenessCentrality(graph);
+            for (String node : graph.getVertices()) {
+                double score = centrality.getVertexScore(node);
+
+                System.out.println(node + " = " + score);
+
+                if(score > maxValue) {
+                    maxNode = node;
+                    maxValue = score;
+                }
+            }
+
+            System.out.println("Max node = " + maxNode);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+        GraphAnalyser analyser = new GraphAnalyser();
+        analyser.analyseGraph();
+
+    }
+
+
+
+}
